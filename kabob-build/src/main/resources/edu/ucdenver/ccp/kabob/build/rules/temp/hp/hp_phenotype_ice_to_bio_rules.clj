9c6727cd88005a8097107b9bf82b05ae46621ecf
bill-baumgartner <bill.baumgartner@gmail.com>
2016-07-05 12:52:55 -0600

    initial commit of hp phenotype annotation ice-to-bio rule

---
+++ kabob-build/src/main/resources/edu/ucdenver/ccp/kabob/build/rules/temp/hp/hp_phenotype_ice_to_bio_rules.clj
@@ -0,0 +1,29 @@
+`{:name "hp-phentotype"
+  :head (
+         (?/pheno rdfs/subClassOf ?/hp)
+         (?/gene rdfs/subClassOf ?/bioentity)
+
+         (?/hr1 rdf/type owl/Restriction)
+         (?/hr1 owl/onProperty obo/RO_0000057) ; has_participant
+         (?/hr1 owl/someValuesFrom ?/gene)
+
+         (?/pheno rdfs/subClassOf ?/hr1))
+
+  :body ((?/geneIdField kiao/hasTemplate iaohp/HpAnnotationFileRecord_geneIdDataField1)
+  (?/geneIdField obo/IAO_0000219 ?/geneIce)
+  (?/geneIce obo/IAO_0000219 ?/bioentity)
+  (?/record obo/BFO_0000051 ?/geneIdField)
+  (?/record obo/BFO_0000051 ?/phenoIdField)
+  (?/phenoIdField kiao/hasTemplate iaohp/HpAnnotationFileRecord_hpoTermDataField1)
+  (?/phenoIdField obo/IAO_0000219 ?/phenoIce)
+  (?/phenoIce obo/IAO_0000219 ?/pheno))
+
+  :reify ([?/pheno {:ln (:sha-1 ?/pheno ?/hr1)
+                 :ns "kbio" :prefix "PHENO_"}]
+          [?/gene {:ln (:sha-1 ?/pheno ?/bioentity)
+                    :ns "kbio" :prefix "G_"}]
+          [?/hr1 {:ln (:restriction)
+                 :ns "kbio" :prefix "RESTR_"}])
+
+  :options {:magic-prefixes [["franzOption_clauseReorderer" "franz:identity"]]}
+  }
