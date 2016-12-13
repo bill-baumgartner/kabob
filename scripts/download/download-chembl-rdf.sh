29195060308f4756895568f936a92accc61d467b
bill-baumgartner <bill.baumgartner@gmail.com>
2016-12-13 08:28:37 -0700

    add scripts for downloading chembl and irefweb data files

---
+++ scripts/download/download-chembl-rdf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# ChEMBL is availabe as RDF. This script downloads the ChEMBL turtle RDF files.
+#
+# NOTE: due to CD'ing in script use absolute file names!!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+EXPECTED_ARGS=2
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "#NOTE: due to CD'ing in script use absolute file names!!"
+    echo "Usage: LOG_FILE TARGET_DOWNLOAD_DIR"
+    echo "current usage:"
+    echo $@
+    exit 1
+fi
+
+LOG_FILE=$1
+TARGET_DIR=$2
+
+echo "Logging to: $LOG_FILE"
+echo "Saving files to: $TARGET_DIR"
+
+mkdir -p $TARGET_DIR
+cd $TARGET_DIR
+
+wget -r -nd -l1 --no-parent -A.gz ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBL-RDF/latest/
+
+# remove an extraneous line break on lines 16185-6
+gunzip -c chembl_22.1_cellline.ttl.gz | sed 's/6286$/6286> ;/' | sed '/^> ;/d' | gzip -c > chembl_22.1_cellline.FIXED.ttl.gz
+rm chembl_22.1_cellline.ttl.gz
---
+++ scripts/download/download-irefweb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# most downloads are handled in the Java code, however the irefweb downloads sometimes terminate
+# prematurely so we use unix utilities to download the irefweb files
+#
+# NOTE: due to CD'ing in script use absolute file names!!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+EXPECTED_ARGS=2
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "#NOTE: due to CD'ing in script use absolute file names!!"
+    echo "Usage: LOG_FILE TARGET_DOWNLOAD_DIR"
+    echo "current usage:"
+    echo $@
+    exit 1
+fi
+
+LOG_FILE=$1
+TARGET_DIR=$2
+
+echo "Logging to: $LOG_FILE"
+echo "Saving files to: $TARGET_DIR"
+
+URL="http://irefindex.org/download/irefindex/data/archive/release_14.0/psi_mitab/MITAB2.6/9606.mitab.07042015.txt.zip"
+
+exit_code=0
+echo "downloading $url"
+$DIR/download-and-log.sh $LOG_FILE $TARGET_DIR $url
+e=$?
+if [ $e -ne 0 ]; then
+    exit $e
+fi
+
+# unpack the download
+cd $TARGET_DIR && { unzip -o 9606.mitab.07042015.txt.zip ; touch 9606.mitab.04072015.txt.ready ; cd - ; }
