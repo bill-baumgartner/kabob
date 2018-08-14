dbb9273de3bd4c624ea83acde708e7bf9c35f480
bill-baumgartner <bill.baumgartner@gmail.com>
2018-08-14 09:49:25 -0600

    Squash - rule tuning

--- resources/rules/_1_post_identifier_merge/step_h_ice_to_bio/step_hd_bioentity_linking/instance_based_kr/bioplex/step_a/bioplex_interactions_instance_based.clj
+++ resources/rules/_1_post_identifier_merge/step_h_ice_to_bio/step_hd_bioentity_linking/instance_based_kr/bioplex/step_a/bioplex_interactions_instance_based.clj
@@ -26,7 +26,7 @@
     PREFIX obo: <http://purl.obolibrary.org/obo/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    select ?record ?bioentity1 ?bioentity2 ?physical_association {
+    select ?record ?bioentity1 ?bioentity2 ?physical_association ?has_participant {
 
      {
       select ?physical_association {
